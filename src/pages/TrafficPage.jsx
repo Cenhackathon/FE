0a97e465d4fe0c08706248634266d5531ea181d7
jsh10521 <jsh10521@naver.com>
2025-08-21 16:03:16 +0900

    교통 수정

--- src/pages/TrafficPage.jsx
+++ src/pages/TrafficPage.jsx
@@ -4,14 +4,20 @@ import '../styles/TrafficPageStyles.css';
 import Tmap from '../components/traffic/Tmap';
 
 const TrafficPage = () => {
-  const [posts, setPosts] = useState([]);
-  const [] = useState([]);
-  const [] = useState([]);
+  const [posts, setPosts] = useState([]); // 교통혼잡도 top3
+  const [prediction, setPrediction] = useState([]); // 예측 데이터
+  const [alerts, setAlerts] = useState([]); // 실시간 알림
 
-  const getPosts = async() => {
+  const getPosts = async() => { // 백엔드 보고 맞춰야함
     try{
-      const response = await axios.get('주소들어갈자리');
+      const response = await axios.get('http://127.0.0.1:8000/');
+      const data = response.data;
       console.log('응답완료');
+
+      // api 명세서 보고 수정(.~~부분만 수정)
+      // setPosts(data.posts || []);
+      // setPrediction(data.prediction || []);
+      // setAlerts(data.alerts || []);
       
     } catch (error) {
       console.log('에러: ', error);
@@ -39,12 +45,12 @@ const TrafficPage = () => {
 
         <h3>교통 혼잡도 TOP3</h3>
         <ul className="legend-list">
-          {/* {posts.map((post, index) => (
+          {posts.map((post, index) => (
             <li key={index}>
-              {`${index + 1}. `}{post.location} >>>>여기 백엔드 주소에 따라 바뀌어야함
+              {`${index + 1}. `}{post.location} {/*>>>>여기 백엔드 주소에 따라 바뀌어야함*/}
             </li>
-          ))} */}
-          <li>
+          ))}
+          {/* <li>
             1. 이문동(주소)
           </li>
           <li>
@@ -52,33 +58,32 @@ const TrafficPage = () => {
           </li>
           <li>
             3. 휘경동(주소)
-          </li>
+          </li> */}
         </ul>
 
         <h3>예측 데이터</h3>
         <ul className="legend-list">
             <p className="prediction-text">
-              도로 혼잡 예상 구간: 청량리역, 장안동 사거리(주소) {/*{prediction.join(', ')} */}
+              도로 혼잡 예상 구간: 청량리역, 장안동 사거리(주소) {prediction.join(', ')}
             </p>
         </ul>
 
         <h3>실시간 알림</h3>
         <ul className="legend-list">
-          {/* {alerts.map((alert, index) => (
+          {alerts.map((alert, index) => (
             <div 
             key = {index} 
             className = {alert.type === 'warning' ? "alert-box-red" : "alert-box-yellow"}>
               {alert.type === 'warning' ? "🚨 " : "🚧 "}
               {alert.message}
             </div>
-          ))} */}  {/* 백주소에 맞게 변환 */}
-            <div className="alert-box-red">
+          ))}  {/* 백주소에 맞게 변환 */}
+            {/* <div className="alert-box-red">
                 🚨 회기동 사거리 차량 사고로 인한 정체(주소)
             </div>
-
             <div className="alert-box-yellow">
                  🚧 외대 앞 사거리 공사로 인한 정체(주소)
-            </div>
+            </div> */}
         </ul>
       </div>
     </div>
